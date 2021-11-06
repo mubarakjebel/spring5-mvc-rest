d184306d16191c830ebf40b26db886f023371fec
mubarekshejebel <mubarek.shejebel@mint.gov.et>
2021-11-06 17:32:54 -0500

    added model/api, domain, repositories for Category

---
+++ src/main/java/guru/springframework/spring5mvcrest/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring5mvcrest.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/guru/springframework/spring5mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5mvcrest.repositories;
+
+import guru.springframework.spring5mvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
