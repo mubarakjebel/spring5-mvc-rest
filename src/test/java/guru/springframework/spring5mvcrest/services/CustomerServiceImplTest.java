1c286510cfe1cf9094fe36cb0a570736c44e9af3
mubarekshejebel <mubarek.shejebel@mint.gov.et>
2021-11-07 20:14:24 -0600

    added creata new customer

--- src/test/java/guru/springframework/spring5mvcrest/services/CustomerServiceImplTest.java
+++ src/test/java/guru/springframework/spring5mvcrest/services/CustomerServiceImplTest.java
@@ -4,7 +4,6 @@ import guru.springframework.spring5mvcrest.api.v1.mapper.CustomerMapper;
 import guru.springframework.spring5mvcrest.api.v1.model.CustomerDTO;
 import guru.springframework.spring5mvcrest.domain.Customer;
 import guru.springframework.spring5mvcrest.repositories.CustomerRepository;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -14,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -24,13 +24,15 @@ class CustomerServiceImplTest {
 
     CustomerMapper customerMapper = CustomerMapper.INSTANCE;
 
-    CustomerService customerService;
+    CustomerServiceImpl customerService;
 
     @BeforeEach
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-
-        customerService = new CustomerServiceImpl(customerMapper, customerRepository);
+        MockitoAnnotations.openMocks(this);
+        customerService=new CustomerServiceImpl();
+        customerService.setCustomerMapper(customerMapper);
+        customerService.setCustomerRepository(customerRepository);
+        // customerService = new CustomerServiceImpl(customerMapper, customerRepository);
     }
 
     @Test
@@ -70,5 +72,27 @@ class CustomerServiceImplTest {
         CustomerDTO customerDTO = customerService.getCustomerById(1L);
 
         assertEquals("Michale", customerDTO.getFirstname());
+    }
+
+    @Test
+    public void createNewCustomer() throws Exception {
+
+        //given
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstname("Jim");
+
+        Customer savedCustomer = new Customer();
+        savedCustomer.setFirstname(customerDTO.getFirstname());
+        savedCustomer.setLastname(customerDTO.getLastname());
+        savedCustomer.setId(1l);
+
+        when(customerRepository.save(any(Customer.class))).thenReturn(savedCustomer);
+
+        //when
+        CustomerDTO savedDto = customerService.createNewCustomer(customerDTO);
+
+        //then
+        assertEquals(customerDTO.getFirstname(), savedDto.getFirstname());
+        assertEquals("/api/v1/customer/1", savedDto.getCustomerUrl());
     }
 }
