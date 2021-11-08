579d78b5a73fe60af76eaccad84a9735a8d1a03b
mubarekshejebel <mubarek.shejebel@mint.gov.et>
2021-11-08 10:48:48 -0600

    implemented delete customer

--- src/main/java/guru/springframework/spring5mvcrest/controllers/v1/CustomerController.java
+++ src/main/java/guru/springframework/spring5mvcrest/controllers/v1/CustomerController.java
@@ -33,13 +33,13 @@ public class CustomerController {
     }
 
     @PostMapping
-    public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO) {
         return new ResponseEntity<CustomerDTO>(customerService.createNewCustomer(customerDTO),
                 HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO){
+    public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO) {
         return new ResponseEntity<CustomerDTO>(customerService.saveCustomerByDTO(id, customerDTO),
                 HttpStatus.OK);
     }
@@ -48,5 +48,11 @@ public class CustomerController {
     public ResponseEntity<CustomerDTO> patchCustomer(@PathVariable Long id, @RequestBody CustomerDTO customerDTO){
         return new ResponseEntity<CustomerDTO>(customerService.saveCustomerByDTO(id, customerDTO),
                 HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomerById(@PathVariable Long id){
+        customerService.deleteCustomerById(id);
+      return new ResponseEntity<Void>(HttpStatus.OK);
     }
 }
--- src/main/java/guru/springframework/spring5mvcrest/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring5mvcrest/services/CustomerServiceImpl.java
@@ -75,11 +75,11 @@ public class CustomerServiceImpl implements CustomerService {
     public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
         return customerRepository.findById(id).map(customer -> {
 
-            if(customerDTO.getFirstname() != null){
+            if (customerDTO.getFirstname() != null) {
                 customer.setFirstname(customerDTO.getFirstname());
             }
 
-            if(customerDTO.getLastname() != null){
+            if (customerDTO.getLastname() != null) {
                 customer.setLastname(customerDTO.getLastname());
             }
             CustomerDTO returnDto = customerMapper.customerToCustomerDTO(customerRepository.save(customer));
@@ -89,4 +89,10 @@ public class CustomerServiceImpl implements CustomerService {
             return returnDto;
         }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
     }
+
+    @Override
+    public void deleteCustomerById(Long id) {
+        customerRepository.deleteById(id);
+    }
+
 }
