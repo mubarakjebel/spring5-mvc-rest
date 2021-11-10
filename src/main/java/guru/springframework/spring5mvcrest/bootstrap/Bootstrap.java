e892f73ef4fa426cc8be2aa505828da8c0e3dcb1
mubarekshejebel <mubarek.shejebel@mint.gov.et>
2021-11-09 21:11:44 -0600

    added vendor api functionality

--- src/main/java/guru/springframework/spring5mvcrest/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/spring5mvcrest/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5mvcrest.bootstrap;
 
 import guru.springframework.spring5mvcrest.domain.Category;
 import guru.springframework.spring5mvcrest.domain.Customer;
+import guru.springframework.spring5mvcrest.domain.Vendor;
 import guru.springframework.spring5mvcrest.repositories.CategoryRepository;
 import guru.springframework.spring5mvcrest.repositories.CustomerRepository;
+import guru.springframework.spring5mvcrest.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,19 @@ public class Bootstrap implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
     private final CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository, VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         loadCategories();
         loadCustomers();
+        loadVendors();
     }
 
     private void loadCategories() {
@@ -65,5 +70,30 @@ public class Bootstrap implements CommandLineRunner {
         customerRepository.save(customer2);
 
         System.out.println("Customers Loaded: " + customerRepository.count());
+    }
+
+    private void loadVendors() {
+        Vendor vendor1 = new Vendor();
+        vendor1.setName("Western Tasty Fruits Ltd.");
+
+        Vendor vendor2 = new Vendor();
+        vendor2.setName("Exotic Fruits Company");
+
+        Vendor vendor3 = new Vendor();
+        vendor3.setName("Home Fruits");
+
+        Vendor vendor4 = new Vendor();
+        vendor4.setName("Fun Fresh Fruits Ltd.");
+
+        Vendor vendor5 = new Vendor();
+        vendor5.setName("Nuts for Nuts Company");
+
+        vendorRepository.save(vendor1);
+        vendorRepository.save(vendor2);
+        vendorRepository.save(vendor3);
+        vendorRepository.save(vendor4);
+        vendorRepository.save(vendor5);
+
+        System.out.println("Vendors Loaded: " + vendorRepository.count());
     }
 }
---
+++ src/main/java/guru/springframework/spring5mvcrest/services/VendorServiceImpl.java
@@ -0,0 +1,98 @@
+package guru.springframework.spring5mvcrest.services;
+
+import guru.springframework.spring5mvcrest.api.v1.mapper.VendorMapper;
+import guru.springframework.spring5mvcrest.api.v1.model.VendorDTO;
+import guru.springframework.spring5mvcrest.api.v1.model.VendorListDTO;
+import guru.springframework.spring5mvcrest.controllers.v1.VendorController;
+import guru.springframework.spring5mvcrest.domain.Vendor;
+import guru.springframework.spring5mvcrest.repositories.VendorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class VendorServiceImpl implements VendorService {
+
+    private final VendorMapper vendorMapper;
+    private final VendorRepository vendorRepository;
+
+    public VendorServiceImpl(VendorMapper vendorMapper, VendorRepository vendorRepository) {
+        this.vendorMapper = vendorMapper;
+        this.vendorRepository = vendorRepository;
+    }
+
+    @Override
+    public VendorListDTO getAllVendors() {
+        List<VendorDTO> vendorDTOS = vendorRepository
+                .findAll()
+                .stream()
+                .map(vendor -> {
+                    VendorDTO vendorDTO = vendorMapper.vendorToVendorDTO(vendor);
+                    vendorDTO.setVendorUrl(getVendorUrl(vendor.getId()));
+                    return vendorDTO;
+                })
+                .collect(Collectors.toList());
+
+        return new VendorListDTO(vendorDTOS);
+    }
+
+    @Override
+    public VendorDTO getVendorById(Long id) {
+
+        return vendorRepository.findById(id)
+                .map(vendorMapper::vendorToVendorDTO)
+                .map(vendorDTO -> {
+                    vendorDTO.setVendorUrl(getVendorUrl(id));
+                    return vendorDTO;
+                })
+                .orElseThrow(ResourceNotFoundException::new);
+    }
+
+    @Override
+    public VendorDTO createNewVendor(VendorDTO vendorDTO) {
+        return saveAndReturnDTO(vendorMapper.vendorDTOtoVendor(vendorDTO));
+    }
+
+    private VendorDTO saveAndReturnDTO(Vendor vendor) {
+        Vendor savedVendor = vendorRepository.save(vendor);
+
+        VendorDTO returnDto = vendorMapper.vendorToVendorDTO(savedVendor);
+
+        returnDto.setVendorUrl(getVendorUrl(savedVendor.getId()));
+
+        return returnDto;
+    }
+
+    @Override
+    public VendorDTO saveVendorByDTO(Long id, VendorDTO vendorDTO) {
+        Vendor vendor = vendorMapper.vendorDTOtoVendor(vendorDTO);
+        vendor.setId(id);
+        return saveAndReturnDTO(vendor);
+    }
+
+    @Override
+    public VendorDTO patchVendor(Long id, VendorDTO vendorDTO) {
+        return vendorRepository.findById(id)
+                .map(vendor -> {
+                    //todo if more properties, ad more statements
+
+                    if (vendorDTO.getName() != null) {
+                        vendor.setName(vendorDTO.getName());
+                    }
+
+                    VendorDTO returnDto = vendorMapper.vendorToVendorDTO(vendorRepository.save(vendor));
+                    returnDto.setVendorUrl(getVendorUrl(id));
+                    return returnDto;
+                }).orElseThrow(ResourceNotFoundException::new);
+    }
+
+    @Override
+    public void deleteVendorById(Long id) {
+        vendorRepository.deleteById(id);
+    }
+
+    private String getVendorUrl(Long id) {
+        return VendorController.BASE_URL + "/" + id;
+    }
+}
